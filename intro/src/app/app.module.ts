31f255468753b58c3ff442547715cb69b24fd634
Chintan2108 <chintanmaniyar@gmail.com>
2018-12-13 10:21:32 +0530

    added custom pipeline

--- intro/src/app/app.module.ts
+++ intro/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { SqrtPipe } from './app.sqrt';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewCmpComponent
+    NewCmpComponent,
+    SqrtPipe
   ],
   imports: [
     BrowserModule,
